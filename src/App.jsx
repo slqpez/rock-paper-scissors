1dd6f7e16a3f13824d3c6500ce24cd0c5cd7a123
slqpez <santiago.lopez16@udea.edu.co>
2021-07-17 19:07:44 -0500

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App home">
+      <div className="score-section">
+
+      </div>
+      <div className="game-section">
+
+      </div>
+    </div>
+  )
+}
+
+export default App
